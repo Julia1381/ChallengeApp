4bf9a925ee8c9eeb9be2824af183009fa4005c86
Julia Stec <stecjulia7@gmail.com>
2023-08-03 22:56:21 +0200

    Zadanie dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+string name = "Julia";
+var sex = "Woman";
+var age = 17;
+
+
+
+
+if (sex == "Woman" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33) 
+{
+    Console.WriteLine(" Ewa, lat 33");
+}
+else if (sex != "Woman" && age<18)
+{ 
+    Console.WriteLine("Niepełnoletni Mężczyzna ");
+}
